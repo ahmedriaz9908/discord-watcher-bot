1352a5d414c15dbba296e30cf2a8ca35b7d03cf7
riaz-123 <44791965+ahmedriaz9908@users.noreply.github.com>
2019-06-25 13:46:40 -0600

    Update swearcheck.js

--- util/swearcheck.js
+++ util/swearcheck.js
@@ -2,9 +2,13 @@ var discord = require('discord.js');
 var db = require('quick.db');
 
 exports.check = async function(message) {
+  try {
+    
   var swears = await db.get(`swears_${message.guild.id}`)
   for (let i = 0; i < swears.length; i++) {
     if (message.content.toLowerCase().includes(swears[i])) return true;    
   }
+    
+    } catch (e) {}
 }
   
