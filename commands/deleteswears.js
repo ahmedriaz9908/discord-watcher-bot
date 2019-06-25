d6217562b3555e8fe9efc56dd4a702f82d6559fb
riaz-123 <44791965+ahmedriaz9908@users.noreply.github.com>
2019-06-25 13:44:16 -0600

    Update deleteswears.js

--- commands/deleteswears.js
+++ commands/deleteswears.js
@@ -2,10 +2,12 @@ var discord = require('discord.js');
 var db = require('quick.db');
 
 exports.run = async function(client, message, args, prefix) {
-  
+  try {
+    
   if (!message.guild) return;
   if (!message.member.hasPermission(`MANAGE_GUILD`)) return message.channel.send(`Sorry, you need manage guild permission to use this!`)
   await db.delete(`swears_${message.guild.id}`)
   message.channel.send(`OK, deleted all swears for this guild.`)
-  
+    
+  } catch (e) {}
 }
